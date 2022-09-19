637a789b7dfa65769944cfe7b520475dc02e09b8
aubrypau <paul.aubry.ent@gmail.com>
2022-09-19 21:45:39 +0200

    blablabla

---
+++ app/src/main/java/com/btm/info507/ExpenseActivity.kt
@@ -0,0 +1,11 @@
+package com.btm.info507
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExpenseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_expense)
+    }
+}
